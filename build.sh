fcc2b59c24bc7dbf1ede8fdaa0290e81f20ebc41
Producks <davetrap.demers@gmail.com>
2025-04-22 15:22:27 -0400

    Added alt palette and updated linux compliation script, Should be the final build

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # Modified script from the smb2 disassembly https://github.com/Xkeeper0/smb2
-# Last modification: 2025-04-14 Producks
+# Last modification: 2025-04-22 Producks
 
 # Compilation flag
 PROTO_MUSIC="-dPROTOTYPE_DPCM_SAMPLES -dPROTOTYPE_INSTRUMENTS -dPROTOTYPE_MUSIC_STARMAN -dPROTOTYPE_MUSIC_UNDERGROUND -dPROTOTYPE_MUSIC_ENDING"
-WARIO_WALUIGI="-dWARIO_WALUIGI"
 
 build() {
   echo 'Assembling...'
@@ -19,10 +18,7 @@ error () {
 if [ $# -eq 0 ]; then
 	echo '[0] Vanilla music + Merio & Garfield'
 	echo '[1] Prototype music + Merio & Garfield'
-	echo '[2] Vanilla music + Warrio & Waluigi'
-	echo '[3] Prototype music + Warrio & Waluigi'
 	read -p 'Enter a number to select a version: ' input
-
 else
   input="$1"
 fi
@@ -34,15 +30,13 @@ case $input in
 	"1")
 	build $PROTO_MUSIC
 	;;
-	"2")
-	build $WARIO_WALUIGI
-	;;
-	"3")
-	build $PROTO_MUSIC $WARIO_WALUIGI
-	;;
 	*)
-  echo Custom flags compilation
-  build $@
+  if [ $# -eq 0 ]; then
+    error
+  else 
+    echo Custom flags compilation
+    build $@
+  fi
 esac
 
 if [ $? -ne 0 ] ; then # Check if any error happened during the building process
